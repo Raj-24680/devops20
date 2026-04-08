ee50c422fa5054765a9295f17f75ca8ebf23e345
Ojas Sinha <ojassinha14@gmail.com>
2026-04-08 23:30:50 +0530

    Update App.java

--- src/main/java/org/bnmit/demo/App.java
+++ src/main/java/org/bnmit/demo/App.java
@@ -13,8 +13,25 @@ public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
+        // ✅ Frontend (HTML page)
+        server.createContext("/", exchange -> {
+            String html = "<html><body>"
+                    + "<h1>CI/CD Pipeline Demo</h1>"
+                    + "<a href='/greet'>Click to Call API</a>"
+                    + "</body></html>";
+
+            exchange.getResponseHeaders().add("Content-Type", "text/html");
+            exchange.sendResponseHeaders(200, html.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(html.getBytes());
+            os.close();
+        });
+
+        // ✅ API Endpoint
         server.createContext("/greet", exchange -> {
             String response = greet();
+
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
@@ -22,6 +39,6 @@ public class App {
         });
 
         server.start();
-        System.out.println("Server running at http://localhost:8080/greet");
+        System.out.println("Server running at http://localhost:8080/");
     }
 }
